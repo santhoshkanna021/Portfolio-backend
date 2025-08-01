7e6926cb2e19fb326fd9c656d88be4c1f7eef88f
santhoshkanna021 <santhoshkannar802@gmail.com>
2025-08-01 15:14:32 +0530

    second commit

--- routers/router.js
+++ routers/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Contact } = require('../api/controllers');
+const { Contact } = require('../controllers/controllers');
 
 router.post('/', Contact);
 
