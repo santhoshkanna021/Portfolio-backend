7f70f7a161d7c0bb09bcb313665f8472622aad14
santhoshkanna021 <santhoshkannar802@gmail.com>
2025-08-04 15:04:18 +0530

    add

--- server.js
+++ server.js
@@ -13,17 +13,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Root Route
+app.get('/', (req, res) => {
+  res.send('✅ API is running.');
+});
+
 // Routes
-app.use('/', contactRouter);
+app.use('/api/contact', contactRouter);
 
-
-// Start server
+// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
-
-  app.get('/', (req, res) => {
-    res.send('✅ API is running.');
-  });
-  
 });
